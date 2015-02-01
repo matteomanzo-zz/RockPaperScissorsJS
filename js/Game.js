3d94326a3331140f06b9dce4ad14e169f39228b4
matteomanzo <imatteo.manzo@gmail.com>
2015-02-01 17:46:30 +0000

    game has options and players

--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,12 @@
-var Game = function() {
+var Game = function(player1, player2) {
+
   PAIRS = {
     rock: {beats: 'scissors'},
     scissors: {beats: 'paper'},
     paper: {beats: 'rock'}
   };
+
+  this.player1 = player1
+  this.player2 = player2
+
 };
--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,13 +3,18 @@ describe('Game', function() {
   var game;
 
   beforeEach(function() {
-    game = new Game();
+    game = new Game('matt', 'opponent');
   });
 
   describe('by default', function() {
 
     it('should have an hash of options', function() {
       expect(game.PAIRS).not.toEqual(0);
+    });
+
+    it('should have two players', function() {
+      expect(game.player1).not.toBe(null);
+      expect(game.player2).not.toBe(null);
     });
   });
 });
