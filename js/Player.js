adbe70069e5e59e86c549b0b8c50dfab32e5bb81
matteomanzo <imatteo.manzo@gmail.com>
2015-02-01 19:56:30 +0000

    computer generates random numbers

--- js/Player.js
+++ js/Player.js
@@ -2,6 +2,33 @@ var Player = function(name) {
   this.name = name
 };
 
-Player.prototype.picks = function(pick) {
-  return this.pick = pick
+  Player.prototype.picks = function(pick) {
+    return this.pick = pick
+  };
+
+
+var Computer = function() {
+  this.name = 'CPU';
+};
+
+Computer.prototype.picks = function() {
+
+  var CPUchoice = Math.random()
+
+  var randomPick = function() {
+
+    if (CPUchoice < 0.33) {
+      return 'rock';
+    }
+
+    else if (CPUchoice > 0.33 && CPUchoice < 0.66) {
+      return 'paper';
+    }
+
+    else {
+      return 'scissors';
+    }
+  };
+    this.pick = randomPick();
+    return this.pick
 };
--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -11,12 +11,48 @@ describe('Player', function() {
     it('should have a name', function() {
       expect(player.name).toEqual('Matt');
     });
+  });
 
-    describe('picks an option', function() {
+  describe('picks an option', function() {
 
-      it('', function() {
-        expect(player.picks('rock')).toEqual('rock');
-      });
+    it('should return the option he picked', function() {
+      expect(player.picks('rock')).toEqual('rock');
     });
   });
+});
+
+describe('Computer', function() {
+
+  var computer;
+
+  beforeEach(function() {
+    computer = new Computer();
+  });
+
+  describe('by default', function() {
+
+    it('should be named CPU', function() {
+      expect(computer.name).toEqual('CPU');
+    });
+  });
+
+  describe('picks a random option', function() {
+
+    it('should pick rock when generates a number smaller than 0.33', function() {
+      spyOn(Math, 'random').and.returnValue('0.25');
+      expect(computer.picks()).toEqual('rock');
+    });
+
+    it('should pick paper when generates a number between 0.33 and 0.66', function() {
+      spyOn(Math, 'random').and.returnValue('0.50');
+      expect(computer.picks()).toEqual('paper');
+    });
+
+    it('should pick scissors when generates a number bigger than 0.66', function() {
+      spyOn(Math, 'random').and.returnValue(0.80);
+      expect(computer.picks()).toEqual('scissors');
+    });
+
+  });
+
 });
